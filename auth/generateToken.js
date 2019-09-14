780a093a547ff6f0cd9b044574d849f0207370ff
cwern40 <chris.wernli@hotmail.com>
2019-09-14 10:20:56 -0600

    I created the generateToken.js file and added code to genereate the JWT

---
+++ auth/generateToken.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken')
+
+function generateToken(user) {
+
+    const payload = {
+        sub: user.id,
+        username: user.username,
+    };
+
+    const options = {
+        expiresIn: '1d'
+    };
+
+    return jwt.sign(payload, process.env.JWT_SECRET, options)
+}
+
+module.exports = generateToken;
