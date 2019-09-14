13317d96fe7cfc56795fafdeb5dff06203751657
cwern40 <chris.wernli@hotmail.com>
2019-09-14 11:49:28 -0600

    I wrote the tests for the login function

--- auth/auth-model.js
+++ auth/auth-model.js
@@ -6,7 +6,7 @@ module.exports = {
 }
 
 function findBy(filter) {
-    
+    return db('users').where(filter)
 }
 
 function add(user) {
--- auth/auth-model.spec.js
+++ auth/auth-model.spec.js
@@ -7,7 +7,7 @@ describe('the authentication model', () => {
 
     beforeEach(async () => {
         await db('users').truncate();
-    })
+    });
 
     describe('The registration model', () => {
 
@@ -16,13 +16,34 @@ describe('the authentication model', () => {
             const user = await Auth.add(userData);
 
             expect(user.length).toBe(1);
-        })
+        });
 
         it('should return index of 1', async () => {
             const userData = { username: "John", password: "test"};
             const user = await Auth.add(userData)
 
             expect(user).toEqual([1]);
+        });
+    })
+
+    describe('the login model', () => {
+
+        it('should return an object', async () => {
+            const userData = { username: "John", password: "test"};
+            const { username, password } = userData;
+            const user = await Auth.add(userData);
+            const login = await Auth.findBy({ username })
+
+            expect(login.length).toBe(1);
+        })
+
+        it('Should return user data', async () => {
+            const userData = { username: "John", password: "test"};
+            const { username, password } = userData;
+            const user = await Auth.add(userData);
+            const login = await Auth.findBy({ username })
+
+            expect(login[0]).toEqual({ id: 1, username: 'John', password: "test"});
         })
     })
 })
